ac906936503682c18ceb742b533e733383b5a7ef
david <jokutda@whs.school.nz>
2026-09-21 21:20:10 +1200

    fixed main.js

--- main.js
+++ main.js
@@ -21,40 +21,41 @@ function unBlip() {
     document.body.removeChild(pwBtn)
     document.body.removeChild(pwInp)
     console.log("unblipped")
-}
+
+    const arrowBtn = document.querySelector("#arrow-img")
+    const nightBallImg = document.querySelector("#night-ball-img")
+    arrowBtn.addEventListener("click", function () {
+        nightBallImg.src = "images/night_at_bal.jpeg"
+    })
+
+
+    let run = true
+    let ftImages = []
+
+    while (run) {
+        let img = document.querySelector(".ft-img")
+        if (img) {
+            img.classList = ["ft-img-processed"]
+
+            ftImages.push(img)
+        } else {
+            run = false
+        }
+    }
 
 
 
-const arrowBtn = document.querySelector("#arrow-img")
-const nightBallImg = document.querySelector("#night-ball-img")
-arrowBtn.addEventListener("click", function () {
-    nightBallImg.src = "images/night_at_bal.jpeg"
-})
+    for (let i = 0; i < ftImages.length; i++) {
+        ftImages[i].addEventListener("mouseenter", function () {
+            ftImages[i].src = "images/ft_2.jpeg"
+        })
 
+        ftImages[i].addEventListener("mouseleave", function () {
+            ftImages[i].src = "images/ft_1.jpeg"
+        })
 
-let run = true
-let ftImages = []
-
-while (run) {
-    let img = document.querySelector(".ft-img")
-    if (img) {
-        img.classList = ["ft-img-processed"]
-
-        ftImages.push(img)
-    } else {
-        run = false
     }
 }
 
 
 
-for (let i = 0; i < ftImages.length; i++) {
-    ftImages[i].addEventListener("mouseenter", function () {
-        ftImages[i].src = "images/ft_2.jpeg"
-    })
-
-    ftImages[i].addEventListener("mouseleave", function () {
-        ftImages[i].src = "images/ft_1.jpeg"
-    })
-
-}
