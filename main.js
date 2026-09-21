e2b7666a2240d314aca17abc67911369bd71ae90
david <jokutda@whs.school.nz>
2026-09-22 11:25:37 +1200

    modified html

--- main.js
+++ main.js
@@ -1,61 +1,57 @@
-const bodyEl = document.querySelector("#body")
-const bodyElHTML = bodyEl.innerHTML
-bodyEl.innerHTML = ""
-console.log("blipped")
+const body = document.body
+body.classList.add("hidden")
 
 
 
 const pwBtn = document.querySelector("#password-btn")
 const pwInp = document.querySelector("#password-inp")
+const pwCont = document.querySelector("#pw-container")
 
 pwBtn.addEventListener("click", function () {
     if (pwInp.value == "67iloveyou") {
-        unBlip()
+        body.classList.remove("hidden")
+        pwCont.classList.remove("visible")
+        pwCont.classList.add("hidden")
     }
     
 })
 
 
-function unBlip() {
-    bodyEl.innerHTML = bodyElHTML
-    document.body.removeChild(pwBtn)
-    document.body.removeChild(pwInp)
-    console.log("unblipped")
-
-    const arrowBtn = document.querySelector("#arrow-img")
-    const nightBallImg = document.querySelector("#night-ball-img")
-    arrowBtn.addEventListener("click", function () {
-        nightBallImg.src = "images/night_at_bal.jpeg"
-    })
-
-
-    let run = true
-    let ftImages = []
-
-    while (run) {
-        let img = document.querySelector(".ft-img")
-        if (img) {
-            img.classList = ["ft-img-processed"]
-
-            ftImages.push(img)
-        } else {
-            run = false
-        }
-    }
 
 
 
-    for (let i = 0; i < ftImages.length; i++) {
-        ftImages[i].addEventListener("mouseenter", function () {
-            ftImages[i].src = "images/ft_2.jpeg"
-        })
+const arrowBtn = document.querySelector("#arrow-img")
+const nightBallImg = document.querySelector("#night-ball-img")
+arrowBtn.addEventListener("click", function () {
+    nightBallImg.src = "images/night_at_bal.jpeg"
+})
 
-        ftImages[i].addEventListener("mouseleave", function () {
-            ftImages[i].src = "images/ft_1.jpeg"
-        })
 
+let run = true
+let ftImages = []
+
+while (run) {
+    let img = document.querySelector(".unprocessed")
+    if (img) {
+        img.classList = ["ft-img"]
+
+        ftImages.push(img)
+    } else {
+        run = false
     }
 }
+
+
+
+for (let i = 0; i < ftImages.length; i++) {
+ftImages[i].addEventListener("mouseenter", function () {
+    ftImages[i].src = "images/ft_2.jpeg"
+})
+
+ftImages[i].addEventListener("mouseleave", function () {
+    ftImages[i].src = "images/ft_1.jpeg"
+})}
+
 
 
 
